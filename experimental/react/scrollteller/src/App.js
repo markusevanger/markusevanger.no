fcc386434440cf00e6a1c25f23a04ad534d3fcc0
markusevanger <markusevanger@gmail.com>
2023-04-11 22:04:17 +0200

    spinning FERDIG!

---
+++ experimental/react/scrollteller/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScrollTeller from './components/scrollteller';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollTeller />
+    </div>
+  );
+}
+
+export default App;
