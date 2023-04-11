fca491d51eb66a41155f0baa305066180e065159
markusevanger <markusevanger@gmail.com>
2023-04-11 19:08:29 +0200

    sendt laering

---
+++ experimental/react/todo/src/components/FilterButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function FilterButton(props){
+
+    return(
+         <div className="filters btn-group stack-exception">
+             <button type="button" className="btn toggle-btn" aria-pressed="true">
+               <span className="visually-hidden">Show </span>
+               <span>all</span>
+               <span className="visually-hidden"> tasks</span>
+             </button>
+           </div>
+    );
+}
+
+export default FilterButton;
---
+++ experimental/react/todo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const DATA = [
+
+  { id: "todo-0", name:"Eat", completed:true    },
+  { id: "todo-1", name:"Sleep", completed:false },
+  { id: "todo-2", name:"Repeat", completed:true }
+
+];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App tasks={DATA}/>
+);
