ee7b1d75b2b68a8a74267cc1a54f06d71cf1771d
markusevanger <markusevanger@gmail.com>
2023-07-20 21:17:40 +0200

    CV FERDIG???

--- Current/Sommer23_teaser_side/js/epost.js
+++ Current/Sommer23_teaser_side/js/epost.js
@@ -5,12 +5,13 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const kopierEpost = async () => {
     navigator.clipboard.writeText(EPOST);
+    tidligereText = document.getElementById("Epost").innerText
     document.getElementById("Epost").innerText = "Kopiert! 📋";
     console.log("<3");
 
     await delay(2000);
 
-    document.getElementById("Epost").innerText = "Epost";
+    document.getElementById("Epost").innerText = tidligereText;
 
 
  
