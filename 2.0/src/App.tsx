ced71ef21b4822268a9dc82d18bb2be4d0178cb1
markusevanger <markusevanger@gmail.com>
2024-10-13 15:43:04 +0900

    refactor to own components

--- 2.0/src/App.tsx
+++ 2.0/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import ExternalLinkButton from "./componets/Button.tsx";
-import ProjectItem from "./componets/ProjectItem.tsx";
-import { useSnapCarousel } from "react-snap-carousel";
-import { ChevronLeftCircle, ChevronRightCircle, ExternalLink, FileText, FlaskConical, GithubIcon } from "lucide-react";
 import WindowAnimation from "./componets/WindowAnimation.tsx";
+import ProjectCarousel from "./componets/ProjectCarousel.tsx";
 
 function App() {
 
@@ -30,16 +28,6 @@ function App() {
     return () => clearInterval(updateFrequency);
   }, []);
 
-  const { scrollRef, snapPointIndexes, prev, next, goTo, activePageIndex, hasNextPage, hasPrevPage } = useSnapCarousel();
-
-  useEffect(() => {
-    const updateCarousel = () => {
-      goTo((activePageIndex + 1) % snapPointIndexes.size); // Directly use activePageIndex
-    };
-    const updateFrequency = setInterval(updateCarousel, 2000);
-
-    return () => clearInterval(updateFrequency);
-  }, [activePageIndex, snapPointIndexes]); // Dependencies: effect reruns when activePageIndex or snapPointIndexes change
 
 
   // useEffect for autoScroll
@@ -67,14 +55,13 @@ function App() {
 
         <h1 className="mt-4 mb-1 transition-all text-xl font-bold">{t("hero.titleEmoji")} {t("hero.title")}</h1>
 
-        <p className="text-center">
+        <p className="text-center max-w-[350px]">
           {t("hero.description_1")} <span className="font-mono">{age}</span> {t("hero.description_2")}
         </p>
 
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 mt-8">
 
-          <div className="w-full flex flex-col md:flex-row justify-center gap-2">
-
+          <div className="w-full flex flex-col md:flex-row justify-center items-center gap-2">
             <ExternalLinkButton to="cv" type="default">
               CV
             </ExternalLinkButton>
@@ -82,8 +69,6 @@ function App() {
             <a href="#portfolio">
               Portefølje
             </a>
-
-
           </div>
 
           <div onClick={copyMail} className="cursor-pointer">
@@ -101,61 +86,8 @@ function App() {
 
         <section id="portfolio" className="w-full flex-col justify-center mt-40">
           <h2 className="text-center font-bold text-lg mb-2">📦 Prosjekter</h2>
-          <div className="flex items-center gap-2">
-            <button className={`transition-all hover:opacity-100 hover:text-markusRed focus:text-markusRed focus:opacity-100  ${hasPrevPage ? "opacity-40 hover:opacity-100" : "invisible"}`} onClick={() => prev()}><ChevronLeftCircle /></button>
 
-            <ul className="flex scrollbar-none scroll-smooth overflow-x-auto snap-x snap-mandatory gap-10" ref={scrollRef}>
-              <ProjectItem
-                index={1}
-                key={1}
-                snapPointIndexes={snapPointIndexes}
-                title={t("projects.1.title")}
-                subtitle={t("projects.1.subtitle")}
-                body={t("projects.1.description")}
-                imageUrl="projectImages/polaris.webp"
-              >
-                <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.1.buttons.github")}  </ExternalLinkButton>
-                <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><FlaskConical /> {t("projects.1.buttons.demo")} </ExternalLinkButton>
-
-              </ProjectItem>
-
-              <ProjectItem
-                key={2}
-                snapPointIndexes={snapPointIndexes}
-                index={2}
-                title={t("projects.2.title")}
-                subtitle={t("projects.2.subtitle")}
-                body={t("projects.2.description")}
-                imageUrl="projectImages/karbon.webp"
-              >
-                <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.2.buttons.github")}  </ExternalLinkButton>
-                <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><FlaskConical /> {t("projects.2.buttons.demo")} </ExternalLinkButton>
-              </ProjectItem>
-
-              <ProjectItem
-                key={3}
-                snapPointIndexes={snapPointIndexes}
-                index={3}
-                title={t("projects.3.title")}
-                subtitle={t("projects.3.subtitle")}
-                body={t("projects.3.description")}
-                imageUrl="projectImages/pawcast.webp"
-              >
-                <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><FileText /> {t("projects.3.buttons.report")}  </ExternalLinkButton>
-                <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.3.buttons.github")}  </ExternalLinkButton>
-                <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><ExternalLink /> {t("projects.3.buttons.website")} </ExternalLinkButton>
-
-              </ProjectItem>
-            </ul>
-            <button className={`transition-all  hover:text-markusRed focus:text-markusRed focus:opacity-100  ${hasNextPage ? "opacity-40 hover:opacity-100" : "opacity-0  hover:opacity-0"}`} onClick={() => next()}><ChevronRightCircle /></button>
-
-          </div>
-
-          <div className="flex gap-1 justify-center mt-4">
-            {Array.from({ length: snapPointIndexes.size }, (_, i) => (
-              <button onClick={() => goTo(i)} className={`transition-all rounded-full h-2 w-2 bg-markusRed  ${activePageIndex === i ? "scale-125 mx-1" : "opacity-40"}`} key={i}></button>
-            ))}
-          </div>
+          <ProjectCarousel></ProjectCarousel>
 
         </section>
       </div>
---
+++ 2.0/src/componets/ProjectCarousel.tsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import ExternalLinkButton from "./Button";
+import ProjectItem from "./ProjectItem";
+import { useTranslation } from "react-i18next";
+import { useSnapCarousel } from "react-snap-carousel";
+import { ChevronLeftCircle, ChevronRightCircle, ExternalLink, FileText, FlaskConical, GithubIcon } from "lucide-react";
+
+
+
+export default function ProjectCarousel() {
+
+
+    const [t] = useTranslation("main") // used for language
+    const { scrollRef, snapPointIndexes, prev, next, goTo, activePageIndex, hasNextPage, hasPrevPage } = useSnapCarousel();
+    const [carouselAutoRun, setCarouselAutoRun] = useState(true)
+
+    useEffect(() => {
+        const updateCarousel = () => {
+            if (carouselAutoRun) {
+                console.log(`Sliding to next slide: ${activePageIndex}`)
+            };
+            const updateFrequency = setInterval(updateCarousel, 2000);
+            return () => clearInterval(updateFrequency);
+        }
+    }, [activePageIndex, snapPointIndexes, carouselAutoRun]); // Dependencies: effect reruns when activePageIndex or snapPointIndexes change
+
+
+
+    return (
+
+        <>
+            <div className="flex items-center gap-2" onMouseEnter={() => setCarouselAutoRun(false)} onMouseLeave={() => setCarouselAutoRun(true)}>
+                <button tabIndex={0} className={`transition-all hover:opacity-100 hover:text-markusRed focus:text-markusRed focus:opacity-100  ${hasPrevPage ? "opacity-40 hover:opacity-100" : "invisible"}`} onClick={() => prev()}><ChevronLeftCircle /></button>
+
+                <ul className="flex scrollbar-none scroll-smooth overflow-x-auto snap-x snap-mandatory gap-10" ref={scrollRef}>
+                    <ProjectItem
+                        index={1}
+                        key={1}
+                        snapPointIndexes={snapPointIndexes}
+                        title={t("projects.1.title")}
+                        subtitle={t("projects.1.subtitle")}
+                        body={t("projects.1.description")}
+                        imageUrl="projectImages/polaris.webp"
+                    >
+                        <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.1.buttons.github")}  </ExternalLinkButton>
+                        <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><FlaskConical /> {t("projects.1.buttons.demo")} </ExternalLinkButton>
+
+                    </ProjectItem>
+
+                    <ProjectItem
+                        key={2}
+                        snapPointIndexes={snapPointIndexes}
+                        index={2}
+                        title={t("projects.2.title")}
+                        subtitle={t("projects.2.subtitle")}
+                        body={t("projects.2.description")}
+                        imageUrl="projectImages/karbon.webp"
+                    >
+                        <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.2.buttons.github")}  </ExternalLinkButton>
+                        <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><FlaskConical /> {t("projects.2.buttons.demo")} </ExternalLinkButton>
+                    </ProjectItem>
+
+                    <ProjectItem
+                        key={3}
+                        snapPointIndexes={snapPointIndexes}
+                        index={3}
+                        title={t("projects.3.title")}
+                        subtitle={t("projects.3.subtitle")}
+                        body={t("projects.3.description")}
+                        imageUrl="projectImages/pawcast.webp"
+                    >
+                        <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><FileText /> {t("projects.3.buttons.report")}  </ExternalLinkButton>
+                        <ExternalLinkButton openInExternalBrowser={true} to="https://github.com/markusevanger/polaris-sideoversikt"><GithubIcon /> {t("projects.3.buttons.github")}  </ExternalLinkButton>
+                        <ExternalLinkButton type="primary" openInExternalBrowser={true} to="https://sidesjekk.markusevanger.no/"><ExternalLink /> {t("projects.3.buttons.website")} </ExternalLinkButton>
+
+                    </ProjectItem>
+                </ul>
+                <button tabIndex={1} className={`transition-all  hover:text-markusRed focus:text-markusRed focus:opacity-100  ${hasNextPage ? "opacity-40 hover:opacity-100" : "opacity-0  hover:opacity-0"}`} onClick={() => next()}><ChevronRightCircle /></button>
+            </div>
+
+            <div className="flex gap-1 justify-center mt-4">
+                {Array.from({ length: snapPointIndexes.size }, (_, i) => (
+                    <button onClick={() => goTo(i)} className={`transition-all rounded-full h-2 w-2 bg-markusRed  ${activePageIndex === i ? "scale-125 mx-1" : "opacity-40"}`} key={i}></button>
+                ))}
+            </div></>
+    )
+}
