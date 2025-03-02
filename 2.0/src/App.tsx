661caa120689f5c19f748b04eee05d8801f0666c
Markus <markusevanger@gmail.com>
2025-03-02 13:32:40 +0100

    add mobius animation item

--- 2.0/src/App.tsx
+++ 2.0/src/App.tsx
@@ -61,6 +61,7 @@ function App() {
       <div className="flex flex-col gap-2 items-center w-full max-w-[610px]">
         <WindowAnimation></WindowAnimation>
 
+
         <h1 className="mt-4 mb-1 transition-all text-markusRed text-4xl font-[Mogi]"> {t("hero.title")}</h1>
 
         <p className="text-center max-w-[350px]">
@@ -75,6 +76,7 @@ function App() {
             </Link>
           </div>
 
+
         </div>
 
         <section id="portfolio" className="w-full  flex-col justify-center mt-28">
@@ -85,6 +87,11 @@ function App() {
             </h2>
             <ProjectCarousel></ProjectCarousel>
           </div>
+
+          <div className="flex flex-col gap-2 justify-center h-full items-center">
+            <img src={"/v4-red.gif"} className="w-32 h-auto" />
+          </div>
+
 
           <div className="rounded-lg mt-8 p-2">
             <h2 className="text-2xl font-[Mogi] text-markusRed">{t("smallProjects.title")}</h2>
@@ -113,7 +120,8 @@ function App() {
             </div>
           }
 
-          <p className="flex gap-2 items-center">{t("bottom.contactMe")} <MailIcon size={16}/> <span onClick={copyMail} title={t("bottom.clickToCopy")}className="cursor-pointer underline"> markusevanger@gmail.com</span></p>
+
+          <p className="flex gap-2 items-center">{t("bottom.contactMe")} <MailIcon size={16} /> <span onClick={copyMail} title={t("bottom.clickToCopy")} className="cursor-pointer underline"> markusevanger@gmail.com</span></p>
           <p>{t("bottom.madeBy")}</p>
           <div className="">
             <a className="w-fit h-fit mb-10 underline hover:markusRed cursor-pointer" onClick={() => toggleLang()}>
@@ -122,7 +130,7 @@ function App() {
           </div>
 
           <motion.div
-            whileHover={{ scale: 0.7 , rotateY: 180 }} className="w-fit">
+            whileHover={{ scale: 0.7, rotateY: 180 }} className="w-fit">
             <svg className="fill-markusRed h-10" viewBox="0 0 64 57" xmlns="http://www.w3.org/2000/svg">
               <path d="M62.03 25.75C60.3151 27.6208 57.3645 27.7737 55.32 26.36C58.3611 41.9606 51.8603 56.75 35.03 56.75C18.9009 56.75 10.8421 41.3374 10.32 26.98C7.95903 29.3032 4.79725 30.9883 1.85001 28.27C-3.02641 23.7916 4.21946 18.5405 7.15001 15.61C4.68366 12.0475 -1.39513 5.35043 4.19001 1.75999C9.00799 -1.36519 12.3175 4.9868 14.55 8.18999C17.7299 4.95528 22.6122 -1.87248 27.35 2.47999C32.1691 6.90567 25.0912 12.2087 22.19 15.16C26.1089 16.8116 33.0007 19.3411 30.03 24.95C27.8912 28.9861 23.7062 27.1573 20.61 25.58C20.7161 34.5405 23.8335 46.3555 34.71 46.43C46.2136 46.43 46.4662 35.7348 44.93 27.03C42.8619 29.0981 39.6791 29.4461 37.43 27.38C32.8329 23.1582 39.4744 17.9756 42.33 15.12C39.4117 12.0333 33.5238 7.45266 37.66 2.96999C41.9158 -1.71137 46.6949 4.74529 49.62 7.82999C52.6383 4.81167 57.0336 -1.25253 61.63 2.96999C66.258 7.22022 60.0308 12.0092 57.17 14.87C60.708 17.3687 66.0878 21.3505 62.03 25.75Z" />
             </svg>
